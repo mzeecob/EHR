1b3cbeca8d28fa8c96ee6a1a9ee0a5b521030c49
Regis <reshimwe2025@gmail.com>
2020-12-02 21:55:22 +0200

    patient routing

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -2,20 +2,19 @@ import express from 'express';
 import patientController from '../controllers/patientController'
 var router = express.Router();
 
-// router.get("/patients", function(req, res, next) {
-//     const patients = patientController.allPatients()
-//     if (patients) {
-//         res.render("./patients", {'patients': patients})
-//     }
-//     res.render("./patients")
-// })
+router.get("/", async function(req, res, next) {
+    const patients = await patientController.allPatients()
+    if (patients) {
+        console.log(patients);
+        res.render("./patients", {'patients': patients})
+    }
 
-
-// router.get("/patients/:slug", patientController.onePatient)
-
-router.get("/patients", function (req, res, next) {
-    res.render("./patients")
 })
+
+
+router.get("/:slug", patientController.onePatient)
+
+
 
 export default router;
 
--- routes/userLogin.route.js
+++ routes/userLogin.route.js
@@ -3,11 +3,11 @@ import express from 'express';
 import userLogin from '../controllers/userController'
 var router = express.Router();
 
-router.get("/login", function (req, res, next) {
+router.get("/", function (req, res, next) {
     res.render("./login")
 })
 
-router.post("/login", async function (req, res, net) {
+router.post("/", async function (req, res, net) {
     const patient = await userLogin.login(req.body, res)
     if (patient) {
         res.redirect("/patients")
